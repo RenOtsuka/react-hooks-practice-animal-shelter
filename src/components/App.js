745615a2b6ec6a9f52af7b87b168d1dde03343b3
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-05-09 15:19:35 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,42 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function onChangeType(e){
+    setFilters({type: e.target.value});
+    //console.log(filters);
+  }
+
+  function onFindPetsClick(){
+    if(filters.type === "all"){
+      fetch(`http://localhost:3001/pets`)
+      .then(r => r.json())
+      .then(data => setPets(data));
+    }
+    else{
+      fetch(`http://localhost:3001/pets?type=${filters.type}`)
+      .then(r => r.json())
+      .then(data => setPets(data));
+    }
+    
+  }
+
+  function onAdoptPet(id){
+    fetch(`http://localhost:3001/pets/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({isAdopted: true})
+    })
+    .then(r => r.json())
+    .then(data => console.log(data));
+    
+    // const changePet = pets.filter(pet => pet.id === id);
+    // changePet.isAdopted = true;
+    // console.log(changePet);
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +51,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
